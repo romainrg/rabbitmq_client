cf9b7170b2072c2c4273bd93792dd4920d25c2f6
s.lucien_vauthier@santiane.fr <Qa6U3V46qn>
2018-05-08 18:13:31 +0200

    Ajout de tests unitaires

--- config/testing/rabbitmq_client.php
+++ config/testing/rabbitmq_client.php
@@ -5,7 +5,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
  * Config for Rabbit MQ Library
  */
 $config['rabbitmq_client'] = array(
-    'host' => '172.18.0.1',
+    'host' => '172.17.0.1',
     'port' => 5672,
     'user' => 'rabbitmq',
     'pass' => 'rabbitmq',
