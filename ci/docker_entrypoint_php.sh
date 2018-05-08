ffac1ee635440b510c671f4fb8ca21daea22e7c7
s.lucien_vauthier@santiane.fr <Qa6U3V46qn>
2018-05-08 17:52:15 +0200

    Ajout de tests unitaires

---
+++ ci/docker_entrypoint_php.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# We need to install dependencies only for Docker
+[[ ! -e /.dockerenv ]] && exit 0
+
+set -xe
+
+# Install extention
+docker-php-ext-install -j$(nproc) bcmath
+
+exec "$@"
