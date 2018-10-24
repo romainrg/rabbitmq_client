2b42b6499adefb7e0c5008f6272b8e95ea1fbfa3
Romain GALLIEN <r.gallien@santiane.fr>
2018-10-24 10:46:42 +0200

    Update to mirror

--- config/production/constants.php
+++
@@ -1,4 +0,0 @@
-<?php
-defined('BASEPATH') OR exit('No direct script access allowed');
-
-defined('RABBITMQ_CLIENT_PATH') OR define('RABBITMQ_CLIENT_PATH', FCPATH . 'vendor/santiane/rabbitmq_client');
