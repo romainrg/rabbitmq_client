8c824cb122e6ca677cc2cb42aff87df6dcaa0211
Unknown <slucienvauthier@gmail.com>
2019-02-20 11:33:38 +0100

    Debug CI

--- tests/application/config/testing/rabbitmq_client.php
+++ tests/application/config/testing/rabbitmq_client.php
@@ -5,8 +5,8 @@ defined('BASEPATH') OR exit('No direct script access allowed');
  * Config for Rabbit MQ Library
  */
 $config['rabbitmq_client'] = array(
-    'host' => '172.17.0.1',
-    'port' => 5673,
+    'host' => 'rabbitmq',
+    'port' => 5672,
     'user' => 'guest',
     'pass' => 'guest',
     'vhost' => '/',
